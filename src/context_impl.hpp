8b059948e5670d446b57a87f9e2b1333ca14b3b3
Sydney Renee <sydney@solace.ofharmony.ai>
2025-12-11 21:58:26 -0800

    Refactoring of C++ folders continues

--- src/context_impl.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../../../include/kotlinx/coroutines/core_fwd.hpp"
-#include "../../../include/kotlinx/coroutines/CoroutineContext.hpp"
-#include "../../../include/kotlinx/coroutines/Runnable.hpp"
-#include "../../../include/kotlinx/coroutines/Continuation.hpp"
-#include "../../../include/kotlinx/coroutines/ContinuationInterceptor.hpp"
-// #include "kotlinx/coroutines/DispatchedContinuation.hpp" // Cycle breaker
-
-// This file now aggregates the implementation details that were previously inline.
