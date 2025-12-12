3c02960cd9d82fe5cb0e8079cfc6cc8db441d654
Sydney Renee <sydney@solace.ofharmony.ai>
2025-12-12 01:36:35 -0800

    Continuing work on porting and cleaning up

---
+++ src/kotlinx/coroutines/Annotations.hpp
@@ -0,0 +1,71 @@
+#pragma once
+/**
+ * @file Annotations.hpp
+ *
+ * Transliterated from: kotlinx-coroutines-core/common/src/Annotations.kt
+ *
+ * Kotlin annotations have no direct C++ equivalent, so they are represented as
+ * empty marker types for API-surface parity and documentation purposes.
+ */
+
+#include "kotlinx/coroutines/flow/Flow.hpp"
+
+namespace kotlinx::coroutines {
+
+/**
+ * Marks declarations in the coroutines that are **delicate** &mdash;
+ * they have limited use-case and shall be used with care in general code.
+ * Any use of a delicate declaration has to be carefully reviewed to make sure it is
+ * properly used and does not create problems like memory and resource leaks.
+ * Carefully read documentation of any declaration marked as `DelicateCoroutinesApi`.
+ */
+struct DelicateCoroutinesApi {};
+
+/**
+ * Marks declarations that are still **experimental** in coroutines API, which means that the design of the
+ * corresponding declarations has open issues which may (or may not) lead to their changes in the future.
+ * Roughly speaking, there is a chance that those declarations will be deprecated in the near future or
+ * the semantics of their behavior may change in some way that may break some code.
+ */
+struct ExperimentalCoroutinesApi {};
+
+/**
+ * Marks [Flow]-related API as a feature preview.
+ *
+ * Flow preview has **no** backward compatibility guarantees, including both binary and source compatibility.
+ * Its API and semantics can and will be changed in next releases.
+ *
+ * Feature preview can be used to evaluate its real-world strengths and weaknesses, gather and provide feedback.
+ * According to the feedback, [Flow] will be refined on its road to stabilization and promotion to a stable API.
+ *
+ * The best way to speed up preview feature promotion is providing the feedback on the feature.
+ */
+struct FlowPreview {};
+
+/**
+ * Marks declarations that are **obsolete** in coroutines API, which means that the design of the corresponding
+ * declarations has serious known flaws and they will be redesigned in the future.
+ * Roughly speaking, these declarations will be deprecated in the future but there is no replacement for them yet,
+ * so they cannot be deprecated right away.
+ */
+struct ObsoleteCoroutinesApi {};
+
+/**
+ * Marks declarations that are **internal** in coroutines API, which means that should not be used outside of
+ * `kotlinx.coroutines`, because their signatures and semantics will change between future releases without any
+ * warnings and without providing any migration aids.
+ */
+struct InternalCoroutinesApi {};
+
+/**
+ * Marks declarations that cannot be safely inherited from.
+ */
+struct ExperimentalForInheritanceCoroutinesApi {};
+
+/**
+ * Marks declarations that cannot be safely inherited from.
+ */
+struct InternalForInheritanceCoroutinesApi {};
+
+} // namespace kotlinx::coroutines
+
--- src/kotlinx/coroutines/flow/internal/Combine.hpp
+++ src/kotlinx/coroutines/flow/internal/Combine.hpp
@@ -9,91 +9,85 @@
 #include <atomic>
 #include <exception>
 
-namespace kotlinx {
-namespace coroutines {
-namespace flow {
-namespace internal {
+namespace kotlinx::coroutines::flow::internal {
 
-template <typename T1, typename T2, typename R>
-class ZipFlow : public Flow<R> {
-    Flow<T1>* flow1_;
-    Flow<T2>* flow2_;
-    std::function<R(T1, T2)> transform_;
+    template <typename T1, typename T2, typename R>
+    class ZipFlow : public Flow<R> {
+        Flow<T1>* flow1_;
+        Flow<T2>* flow2_;
+        std::function<R(T1, T2)> transform_;
 
-public:
-    ZipFlow(Flow<T1>* f1, Flow<T2>* f2, std::function<R(T1, T2)> t) 
-        : flow1_(f1), flow2_(f2), transform_(t) {}
+    public:
+        ZipFlow(Flow<T1>* f1, Flow<T2>* f2, std::function<R(T1, T2)> t)
+            : flow1_(f1), flow2_(f2), transform_(t) {}
 
-    void collect(FlowCollector<R>* collector) override {
-        // Create channels to decouple producers from consumer
-        auto c1 = channels::createChannel<T1>(channels::Channel<T1>::BUFFERED);
-        auto c2 = channels::createChannel<T2>(channels::Channel<T2>::BUFFERED);
+        void collect(FlowCollector<R>* collector) override {
+            // Create channels to decouple producers from consumer
+            auto c1 = channels::createChannel<T1>(channels::Channel<T1>::BUFFERED);
+            auto c2 = channels::createChannel<T2>(channels::Channel<T2>::BUFFERED);
 
-        // Threads for concurrent collection
-        // TODO: Use CoroutineScope.launch when available
-        std::thread t1([&](){
+            // Threads for concurrent collection
+            // TODO: Use CoroutineScope.launch when available
+            std::thread t1([&](){
+                try {
+                    SendingCollector<T1> sc(c1.get());
+                    flow1_->collect(&sc);
+                    c1->close();
+                } catch (...) {
+                    c1->close(std::current_exception());
+                }
+            });
+
+            std::thread t2([&](){
+                try {
+                    SendingCollector<T2> sc(c2.get());
+                    flow2_->collect(&sc);
+                    c2->close();
+                } catch (...) {
+                    c2->close(std::current_exception());
+                }
+            });
+
+            // Zip loop
             try {
-                SendingCollector<T1> sc(c1.get());
-                flow1_->collect(&sc);
-                c1->close();
+                while (true) {
+                    // Wait for element from first
+                    auto r1 = c1->receive_catching();
+                    if (r1.is_closed()) {
+                        // If flow1 finishes, zip finishes
+                        // Need to cancel flow2?
+                        c2->cancel();
+                        break;
+                    }
+
+                    // Wait for element from second
+                    auto r2 = c2->receive_catching();
+                    if (r2.is_closed()) {
+                        c1->cancel();
+                        break;
+                    }
+
+                    if (r1.is_success() && r2.is_success()) {
+                        R result = transform_(r1.get_or_throw(), r2.get_or_throw());
+                        collector->emit(result);
+                    } else {
+                        break;
+                    }
+                }
             } catch (...) {
-                c1->close(std::current_exception());
+                c1->cancel();
+                c2->cancel();
+                // rethrow or handle?
             }
-        });
 
-        std::thread t2([&](){
-            try {
-                SendingCollector<T2> sc(c2.get());
-                flow2_->collect(&sc);
-                c2->close();
-            } catch (...) {
-                c2->close(std::current_exception());
-            }
-        });
-
-        // Zip loop
-        try {
-            while (true) {
-                // Wait for element from first
-                auto r1 = c1->receive_catching();
-                if (r1.is_closed()) {
-                    // If flow1 finishes, zip finishes
-                    // Need to cancel flow2?
-                    c2->cancel(); 
-                    break;
-                }
-
-                // Wait for element from second
-                auto r2 = c2->receive_catching();
-                if (r2.is_closed()) {
-                    c1->cancel();
-                    break;
-                }
-
-                if (r1.is_success() && r2.is_success()) {
-                    R result = transform_(r1.get_or_throw(), r2.get_or_throw());
-                    collector->emit(result);
-                } else {
-                    break; 
-                }
-            }
-        } catch (...) {
-            c1->cancel();
-            c2->cancel();
-            // rethrow or handle?
+            if (t1.joinable()) t1.join();
+            if (t2.joinable()) t2.join();
         }
+    };
 
-        if (t1.joinable()) t1.join();
-        if (t2.joinable()) t2.join();
+    template <typename T1, typename T2, typename R>
+    Flow<R>* zip_impl(Flow<T1>* flow1, Flow<T2>* flow2, std::function<R(T1, T2)> transform) {
+        return new ZipFlow<T1, T2, R>(flow1, flow2, transform);
     }
-};
 
-template <typename T1, typename T2, typename R>
-Flow<R>* zip_impl(Flow<T1>* flow1, Flow<T2>* flow2, std::function<R(T1, T2)> transform) {
-    return new ZipFlow<T1, T2, R>(flow1, flow2, transform);
 }
-
-} // namespace internal
-} // namespace flow
-} // namespace coroutines
-} // namespace kotlinx
