58c6c53c11bba53068fb758cded71a4767f0a7c6
Sydney Renee <sydney@solace.ofharmony.ai>
2025-12-13 22:32:20 -0800

    await and suspend_cancellable_coroutine: refactor to improve DSL wrappers and remove unused code

--- src/kotlinx/coroutines/dsl/Await.hpp
+++ src/kotlinx/coroutines/dsl/Await.hpp
@@ -1,40 +1,30 @@
 #pragma once
 
 #include "kotlinx/coroutines/Deferred.hpp"
-#include "kotlinx/coroutines/dsl/Suspend.hpp"
 #include <memory>
 
 namespace kotlinx {
 namespace coroutines {
-
-// Extension-like syntax for C++ ?
-// In C++, we often use free functions for this or members.
-// If the goal is to support "await(deferred)", here it is.
-
 namespace dsl {
 
-    // Implementation adapter
+    /**
+     * await - DSL wrapper for awaiting a Deferred result.
+     *
+     * Usage within coroutine_yield:
+     *   coroutine_yield(this, await(deferred, completion));
+     *
+     * @param deferred The Deferred to await
+     * @param continuation The completion continuation
+     * @return COROUTINE_SUSPENDED or result
+     */
     template <typename T>
-    [[suspend]]
     void* await(Deferred<T>& deferred, std::shared_ptr<Continuation<void*>> continuation) {
-        return suspend(deferred.await(continuation.get()));
-    }
-
-    // Frontend Stub
-    // The plugin transforms `await(deferred)` into `await(deferred, cont)`
-    template <typename T>
-    [[suspend]]
-    T await(Deferred<T>& deferred) {
-        // This body is replaced by the plugin.
-        // In a non-plugin build, this would just fail or return T() default?
-        // Since we are validating structure, we leave it empty/dummy or throwing.
-        // But for return type T, we must return something to compile.
-        throw std::runtime_error("await stub called without plugin transformation");
+        return deferred.await(continuation.get());
     }
 
 } // namespace dsl
 
-using dsl::await; // Make it available in coroutines namespace? Or user uses calls dsl::await
+using dsl::await;
 
 } // namespace coroutines
 } // namespace kotlinx
